43088649cacc7953c9af6ad930909ea5329a7591
KirienkoEvgeniy <kirienko.evgeny@gmail.com>
2019-04-04 23:14:56 +0300

    new version

--- src/org/demo/birds/application/BirdStoreApplication.java
+++ src/org/demo/birds/application/BirdStoreApplication.java
@@ -1,13 +1,12 @@
 package org.demo.birds.application;
 
 import org.demo.birds.processor.IInfiniteLoopProcessor;
-import org.demo.birds.store.BirdStore;
 
 public class BirdStoreApplication {
 
     public static void main(String[] args) {
          IInfiniteLoopProcessor userCommandProcessor = null; //создать реальный объект
          userCommandProcessor.processInLoop();
-        BirdStore birdStoreApplication = new BirdStore();
+//        BirdStore birdStoreApplication = new BirdStore();
     }
 }
